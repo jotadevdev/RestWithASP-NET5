83b018edb34316db19e4f06021d7d04871ad59e4
jayson <jayson.cr@gmail.com>
2022-01-18 18:19:39 -0300

    Model e Services/Implementations

---
+++ Rest_API_Net5/API_Rest_Core/API_Rest_Core/Services/Implementations/IPersonServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using API_Rest_Core.Model;
+
+namespace API_Rest_Core.Services.Implementations
+{
+    public interface IPersonServices
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+        List<Person> FindAll();
+    }
+}
